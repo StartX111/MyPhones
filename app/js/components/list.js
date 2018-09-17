44cf091f6a0121a8a538e4d6312328c6c2466521
Viktor <vivat.bbb@gmail.com>
2018-09-17 13:59:04 +0300

    - update sorting

--- app/js/components/list.js
+++ app/js/components/list.js
@@ -7,6 +7,30 @@ class List {
 
     showList(data) {
         let result = '';
+        let sort = document.getElementById('sort-phones');
+
+        switch (sort.options[sort.selectedIndex].value) {
+            case('no sorting'):
+                break;
+            case('age'):
+                data.sort(function(a,b){
+                    
+                });
+                console.log('age');
+
+                break;
+            case('sort-A-Z'):
+                console.log('A-Z');
+                data.reverse();
+                break;
+            case('sort-Z-A'):
+                console.log('Z-A');
+                data.reverse();
+                break;
+            default:
+                break;
+        }
+
         data.forEach((item) => {
             result += this.tmpl(item);
         });
--- app/js/components/page-list.js
+++ app/js/components/page-list.js
@@ -6,45 +6,53 @@ class PageList {
         this.el = options.el;
 
         this.phones = phones;
+        this.localPhones = this.phones;
 
         try{
             this.controls = new Controls({
                 el: this.el.querySelector('[data-component="phones-control"]')
             });
-        } catch (e) {
+        } catch (e) {// add control panel if it does not exist
                this.el.innerHTML = `<div class="container">
-            <div class="row">
-                <div data-component="phones-control" class="col-md-2">
-                    <div class="form-group">
-                        <label for="search-phones">Search:</label>
-                        <input class="form-control" type="text" data-control="search" id="search-phones">
-                        <label for="search-phones">Sorting:</label>
-                        <select class="form-control" id="sort-phones">
-                            <option selected value="age">age</option>
-                            <option value="sort-A-Z">name a-z</option>
-                            <option value="sort-Z-A">name z-a</option>
-                        </select>
-                    </div>
-                </div>
-    
-                <div class="col-md-9 offset-1">
-                    <ul data-component="phones-list"></ul>
-                </div>
-            </div>
-        </div>`;
+                        <div class="row">
+                            <div data-component="phones-control" class="col-md-2">
+                                <div class="form-group">
+                                    <label for="search-phones">Search:</label>
+                                    <input class="form-control" type="text" data-control="search" id="search-phones">
+                                    <label for="search-phones">Sorting:</label>
+                                    <select class="form-control" id="sort-phones">
+                                        <option selected value="age">age</option>
+                                        <option value="sort-A-Z">name a-z</option>
+                                        <option value="sort-Z-A">name z-a</option>
+                                    </select>
+                                </div>
+                            </div>
+                
+                            <div class="col-md-9 offset-1">
+                                <ul data-component="phones-list"></ul>
+                            </div>
+                        </div>
+                    </div>`;
+            this.controls = new Controls({
+                el: this.el.querySelector('[data-component="phones-control"]')
+            });
         }
 
-        this.controls = new Controls({
-            el: this.el.querySelector('[data-component="phones-control"]')
-        });
+
 
         this.list = new List({
             el: this.el.querySelector('[data-component="phones-list"]'),
             tmpl: document.getElementById('phone-item-tmpl')
         });
 
-        this.list.showList(this.phones);
+        this.list.showList(this.localPhones);
+
         this.controls.el.addEventListener('search', this.search.bind(this));
+
+        this.selectSort = document.getElementById('sort-phones');
+        this.selectSort.addEventListener('change', ()=>{
+            this.list.showList(this.localPhones);
+        });
     }
 
 
@@ -56,8 +64,10 @@ class PageList {
             const filteredPhones = this.phones.filter((phone) => {
                 return re.test(phone.name);
             });
+            this.localPhones = filteredPhones;
             this.list.showList(filteredPhones);
         } else {
+            this.localPhones = this.phones;
             this.list.showList(this.phones);
         }
     }
